dc8dd09cd264bad20e369f90d8218275db61e149
coosbaakman <coos.baakman@radboudumc.nl>
2024-03-18 11:07:12 +0000

    make the data collatable

--- diffusionmodel/data.py
+++ diffusionmodel/data.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from math import log
+from typing import Optional
 
 import numpy
 import h5py
@@ -10,22 +11,31 @@ from torch.utils.data import Dataset
 
 class MhcpDataset(Dataset):
 
+    protein_size = 200
+    peptide_size = 9
+
     keys = ["aatype", "atom14_gt_exists", "atom14_gt_positions"]
     treshold = 1.0 - log(500) / log(500000)
 
-    def __init__(self, hdf5_path: str):
+    def __init__(self, hdf5_path: str, device: Optional[torch.device] = None):
+
+        if device is not None:
+            self.device = device
+        else:
+            self.device = torch.device("cpu")
+
         self.hdf5_path = hdf5_path
         with h5py.File(self.hdf5_path, 'r') as f5:
             self.entry_names = list(f5.keys())
 
     def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
-        return self._get_entry(self.hdf5_path, self.entry_names[index])
+        return self._get_entry(self.hdf5_path, self.entry_names[index], self.device)
 
     def __len__(self) -> int:
         return len(self.entry_names)
 
     @staticmethod
-    def _get_entry(hdf5_path: str, entry_name: str) -> Dict[str, torch.Tensor]:
+    def _get_entry(hdf5_path: str, entry_name: str, device: torch.device) -> Dict[str, torch.Tensor]:
 
         data = {}
         with h5py.File(hdf5_path, 'r') as f5:
@@ -35,9 +45,14 @@ class MhcpDataset(Dataset):
             protein = entry["protein"]
 
             for key in MhcpDataset.keys:
-                data[f"peptide_{key}"] = torch.tensor(peptide[key][()])
+                t = torch.tensor(peptide[key][()], device=device)
+                data[f"peptide_{key}"] = torch.zeros([MhcpDataset.peptide_size] + list(t).shape[1:], device=device)
+                data[f"peptide_{key}"][:t.shape[0], ...] = t
+                
 
             for key in MhcpDataset.keys:
-                data[f"protein_{key}"] = torch.tensor(protein[key][()])
+                t = torch.tensor(protein[key][()], device=device)
+                data[f"protein_{key}"] = torch.zeros([MhcpDataset.protein_size] + list(t).shape[1:], device=device)
+                data[f"protein_{key}"][:t.shape[0], ...] = t
 
         return data
