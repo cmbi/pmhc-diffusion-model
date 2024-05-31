e7fb8939a55c2a1cfc0fac80b7981d7cbbff49be
cbaakman <c.baakman@umcn.nl>
2024-05-31 16:12:21 +0200

    make the sample script that can take any peptide as input

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ import logging
 
 import torch
 from openfold.utils.rigid_utils import Rigid
+from openfold.np.residue_constants import restypes
 
 from diffusion.optimizer import DiffusionModelOptimizer
 from diffusion.model import Model
@@ -18,12 +19,14 @@ _log = logging.getLogger(__name__)
 
 arg_parser = ArgumentParser()
 arg_parser.add_argument("model")
-arg_parser.add_argument("test_hdf5")
-arg_parser.add_argument("id")
+arg_parser.add_argument("sequence")
 
 if __name__ == "__main__":
 
     args = arg_parser.parse_args()
+    max_len = 16
+    if len(args.sequence) > max_len:
+        raise RuntimeError(f"input sequence is too long, max {max_len} amino acids")
 
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -32,41 +35,40 @@ if __name__ == "__main__":
         device = torch.device("cuda")
 
     # init
-    test_dataset = MhcpDataset(args.test_hdf5, device)
-
     _log.debug(f"initializing model")
     T = 1000
-    model = Model(16, 22, T).to(device=device)
+    H = 22
+    model = Model(max_len, H, T).to(device=device)
     if os.path.isfile(args.model):
         model.load_state_dict(torch.load(args.model, map_location=device))
 
     _log.debug(f"initializing diffusion model optimizer")
     dm = DiffusionModelOptimizer(T, model)
 
-    # sample
     model.load_state_dict(torch.load(args.model, map_location=device))
 
-    test_dataset = MhcpDataset(args.test_hdf5, device)
-    test_entry = test_dataset.get_entry(args.id)
+    # build input data
+    mask = torch.zeros(max_len, device=device, dtype=torch.bool)
+    mask[:len(args.sequence)] = True
 
-    # for sampling, make a batch of size 1
-    test_entry = {key: test_entry[key].unsqueeze(0) for key in test_entry}
+    one_hot_sequence = torch.nn.functional.one_hot(torch.tensor([restypes.index(args.sequence[i])
+                                                                 for i in range(len(args.sequence))],
+                                                                device=device),
+                                                   num_classes=H)
+    features = torch.zeros(max_len, H, device=device)
+    features[mask] = one_hot_sequence.float()
 
-    true_frames = Rigid.from_tensor_7(test_entry["frames"])
-    frame_dimensions = list(range(len(true_frames.shape)))
-
-    input_frames = dm.gen_noise(true_frames.shape, device=device)
+    input_frames = dm.gen_noise([max_len], device=device)
 
     batch = {
-        "frames": input_frames,
-        "mask": test_entry["mask"],
-        "features": test_entry["features"],
+        "frames": input_frames.unsqueeze(0),
+        "mask": mask.unsqueeze(0),
+        "features": features.unsqueeze(0),
     }
 
-    save(true_frames[0], test_entry["mask"][0], f"{args.id}-true.pdb")
-    save(input_frames[0], test_entry["mask"][0], f"{args.id}-input.pdb")
+    save(input_frames, mask, f"{args.sequence}-input.pdb")
 
     with torch.no_grad():
         pred_frames = dm.sample(batch)
 
-    save(pred_frames[0], test_entry["mask"][0], f"{args.id}-output.pdb")
+    save(pred_frames[0], mask, f"{args.sequence}-output.pdb")
---
+++ test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from argparse import ArgumentParser
+import logging
+
+import torch
+from openfold.utils.rigid_utils import Rigid
+
+from diffusion.optimizer import DiffusionModelOptimizer
+from diffusion.model import Model
+from diffusion.data import MhcpDataset
+from diffusion.tools.pdb import save
+
+
+_log = logging.getLogger(__name__)
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument("model")
+arg_parser.add_argument("test_hdf5")
+arg_parser.add_argument("id")
+
+if __name__ == "__main__":
+
+    args = arg_parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+
+    # init
+    _log.debug(f"initializing model")
+    T = 1000
+    model = Model(16, 22, T).to(device=device)
+    if os.path.isfile(args.model):
+        model.load_state_dict(torch.load(args.model, map_location=device))
+
+    _log.debug(f"initializing diffusion model optimizer")
+    dm = DiffusionModelOptimizer(T, model)
+
+    # sample
+    model.load_state_dict(torch.load(args.model, map_location=device))
+
+    test_dataset = MhcpDataset(args.test_hdf5, device)
+    test_entry = test_dataset.get_entry(args.id)
+
+    # for sampling, make a batch of size 1
+    test_entry = {key: test_entry[key].unsqueeze(0) for key in test_entry}
+
+    true_frames = Rigid.from_tensor_7(test_entry["frames"])
+    frame_dimensions = list(range(len(true_frames.shape)))
+
+    input_frames = dm.gen_noise(true_frames.shape, device=device)
+
+    batch = {
+        "frames": input_frames,
+        "mask": test_entry["mask"],
+        "features": test_entry["features"],
+    }
+
+    save(true_frames[0], test_entry["mask"][0], f"{args.id}-true.pdb")
+    save(input_frames[0], test_entry["mask"][0], f"{args.id}-input.pdb")
+
+    with torch.no_grad():
+        pred_frames = dm.sample(batch)
+
+    save(pred_frames[0], test_entry["mask"][0], f"{args.id}-output.pdb")
