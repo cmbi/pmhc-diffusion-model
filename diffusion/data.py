fix the masking for the torsions: chi1 should not be masked out

diffusion/data.py
@@ -91,14 +91,16 @@ class MhcpDataset(Dataset):
             pocket_onehot[:pocket_n_res] = torch.tensor(mhc['sequence_onehot'][:], device=self.device)[mhc_pocket_mask]
 
             # torsion angles: pre-omega, phi, psi, chi-1, chi-2, chi-3, chi-4
-            torsions = torch.zeros(MhcpDataset.peptide_maxlen, 7, 2, device=self.device)
+            torsions = torch.empty(MhcpDataset.peptide_maxlen, 7, 2, device=self.device)
             torsions[:peptide_len] = torch.tensor(peptide['torsion_angles_sin_cos'][:], device=self.device)
             torsions_mask = torch.zeros(MhcpDataset.peptide_maxlen, 7, device=self.device, dtype=torch.bool)
             torsions_mask[:peptide_len] = torch.tensor(peptide['torsion_angles_mask'][:], device=self.device)
             # The frames determine the backbone structure,
             # so disable backbone torsions, except for the C-terminus.
-            torsions_mask[:, :4] = False
+            torsions_mask[:, :3] = False
             torsions_mask[peptide_len - 1, 2] = True
+            # set identity, where masked
+            torsions[torch.logical_not(torsions_mask)] = torch.tensor([0.0, 1.0], device=self.device)
 
             # output dict
             data['mask'] = mask